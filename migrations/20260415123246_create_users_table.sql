eee9ccd4d114b5ccbbf0b5033edd0505411faa9b
Maxim Tretyakov <tretiakov.m@bookcentre.ru>
2026-04-15 15:35:21 +0300

    [feature/user-migration] Создал таблицу пользователей

---
+++ migrations/20260415123246_create_users_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+ );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+-- +goose StatementEnd
